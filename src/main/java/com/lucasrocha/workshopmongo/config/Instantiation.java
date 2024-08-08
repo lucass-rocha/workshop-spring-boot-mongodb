fb7c586c2aa3f1dac259748958f24a08a1723442
Lucas Rocha <lucasrocha478@gmail.com>
2024-08-08 10:48:40 -0300

    Usando o padrão DTO para retornar usuários

--- src/main/java/com/lucasrocha/workshopmongo/config/Instantiation.java
+++ src/main/java/com/lucasrocha/workshopmongo/config/Instantiation.java
@@ -24,6 +24,7 @@ public class Instantiation implements CommandLineRunner {
 		User bob = new User(null, "Bob Grey", "bob@gmail.com");
 		
 		userRepository.saveAll(Arrays.asList(maria,alex,bob));
+		
 	}
 
 }
