43754e3f454da55591d46b1a8f1e39cc210f7fd1
Lucas Rocha <lucasrocha478@gmail.com>
2024-08-19 19:35:34 -0300

    Referenciando os posts do usuario

--- src/main/java/com/lucasrocha/workshopmongo/repository/PostRepository.java
+++ src/main/java/com/lucasrocha/workshopmongo/repository/PostRepository.java
@@ -4,7 +4,6 @@ import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
 import com.lucasrocha.workshopmongo.domain.Post;
-import com.lucasrocha.workshopmongo.domain.User;
 
 @Repository
 public interface PostRepository extends MongoRepository<Post, String>{
